dea051f147cb14da123bb82d9573e8a19518ca30
Arcane <40317108+TheRealArcane@users.noreply.github.com>
2019-04-21 19:57:19 +0200

    Update BasePlayer.js

--- src/client/voice/player/BasePlayer.js
+++ src/client/voice/player/BasePlayer.js
@@ -11,7 +11,12 @@ const FFMPEG_ARGUMENTS = [
   '-f', 's16le',
   '-ar', '48000',
   '-ac', '2',
+  '-reconnect', '1',
+  '-reconnect_streamed', '1',
+  '-reconnect_at_eof', '1',
+  '-reconnect_delay_max', '10',
 ];
+
 
 /**
  * An Audio Player for a Voice Connection.
